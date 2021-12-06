ce78d8062f415e7e1e9860b9251b94d8ed9d9990
mejiaSoc4 <mejiaoscar43@gmail.com>
2021-12-05 18:19:16 -0600

    Agregar a Carrito aun mas debug :D

--- app/src/main/java/com/example/aestheticcakes/CarritoView.java
+++ app/src/main/java/com/example/aestheticcakes/CarritoView.java
@@ -7,22 +7,26 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.denzcoskun.imageslider.interfaces.ItemClickListener;
+//import com.example.aestheticcakes.ItemClickListener;
 
 public class CarritoView extends RecyclerView.ViewHolder implements View.OnClickListener {
-    private ItemClickListener itemClickListener;
     public TextView carProductoNombre, carProductoCantidad, carProductoPrecio;
+    private ItemClickListener itemClickListener;
+
     public CarritoView(@NonNull View itemView) {
         super(itemView);
-        carProductoNombre = itemView.findViewById(R.id.txtNombre);
-        carProductoCantidad = itemView.findViewById(R.id.txtCantidad);
-        carProductoPrecio =itemView.findViewById(R.id.txtPrecio);
+        /*carProductoNombre = itemView.findViewById(R.id.cart_product_name);
+        carProductoCantidad = itemView.findViewById(R.id.cart_product_price);
+        carProductoPrecio =itemView.findViewById(R.id.cart_product_quantity);*/
     }
 
     @Override
     public void onClick(View view) {
 
+        //itemClickListener.onClick(view, getAdapterPosition(),false);
     }
+
     public void setItemClickListener(ItemClickListener itemClickListener){
-        this.itemClickListener=itemClickListener;
+        this.itemClickListener = itemClickListener;
     }
 }
